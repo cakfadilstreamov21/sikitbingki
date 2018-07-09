256d37dd952b3b67eaba4fb68a87c064c466cb90
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-07-09 17:25:09 +0700

    Update sikitbingki1.js

--- sikitbingki1.js
+++ sikitbingki1.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://cakfadil.blogspot.com/2018/07/cara-membuat-favicon-tanpa-ribet-dan.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://cakfadil.blogspot.com/2018/06/tutorial-lengkap-menampilkan-jenis-iklan-link-google-adsense-pada-website.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
