cc2b19cf8fd023a5434fd3dbd484d49ae04d28c2
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-07-20 03:28:29 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+//<![CDATA[
+function changeLink() {
+    var e = Base64.decode(res);
+    window.open(e, "_blank")
+}
+var currentURL = location.href,
+    str = currentURL,
+    res = str.replace("https://sikitbingki.blogspot.com/2018/07/kegunaan-crypto-dalam-ekonomi-dunia.html?url=", "");
+document.write('<button target="_self" class="button" style="display: none;" id="download" onclick="changeLink()"></button>');
+onload: generate();
+//]]>
+</script>
